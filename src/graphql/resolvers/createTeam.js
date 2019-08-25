c260537e10975babec28825bc0275134461eecaf
Efe <knowledgeagare157@gmail.com>
2019-08-25 13:34:17 +0100

    add authentication

--- src/graphql/resolvers/createTeam.js
+++ src/graphql/resolvers/createTeam.js
@@ -1,23 +1,24 @@
 import { formatErrors } from '../../utils/formatError';
-
+import { requiresAuth } from '../../utils/permissions';
 
 export default {
   Query: {},
 
   Mutation: {
-    createTeam: async (root, args, { models, user }, info) => {
-      try {
-        // console.log(user)
-        const team = await models.Team.create({ ...args, owner: user.id });
-        // console.log(team)
-        return { ok: true, team };
-      } catch (error) {
-        
-        return {
-          ok: false,
-          errors: formatErrors(error, models)
-        };
+    createTeam: requiresAuth.createResolver(
+      async (root, args, { models, user }, info) => {
+        try {
+          // console.log(user)
+          const team = await models.Team.create({ ...args, owner: user.id });
+          // console.log(team)
+          return { ok: true, team };
+        } catch (error) {
+          return {
+            ok: false,
+            errors: formatErrors(error, models)
+          };
+        }
       }
-    }
+    )
   }
 };
--- src/index.js
+++ src/index.js
@@ -36,7 +36,6 @@ const server = new ApolloServer({
   resolvers,
   playground: true,
   context: async ({ req, res }) => {
-    console.log(req.headers)
     const token = req.headers['x-token'] || '';
     const refreshToken = req.headers['x-refresh-token'] || '';
     const user = await getUser(token, refreshToken, secret, models, res);
