715a8a1645ccf6a184beea142cffc71291460e9c
Efe <knowledgeagare157@gmail.com>
2019-10-11 18:02:16 +0100

    graphQl subscription with redis

--- src/graphql/pubsub/pubsub.js
+++ src/graphql/pubsub/pubsub.js
@@ -1,3 +1,10 @@
-import {  PubSub } from 'graphql-subscriptions';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
 
-export default new PubSub()
+export default new RedisPubSub({
+  host: '127.0.0.1',
+  port: 6379,
+  retryStrategy: times => {
+    // reconnect after
+    return Math.min(times * 50, 2000);
+  }
+})
