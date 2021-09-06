1facd47c71ed6e5a7f647163f6bf5b25bb6dfc4a
Efe Agare <knowledgeagare157@gmail.com>
2021-09-06 18:03:26 +0100

    edit the name

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,7 +1,7 @@
 import user from './user';
 import workSpace from './workSpace';
 import channel from './channel';
-import workspacemember from './workSpaceMember';
+import workspacemember from './workspacemember';
 import message from './message';
 import directmessage from './directMessage';
 
